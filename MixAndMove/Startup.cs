ff5e4867320af4d039cade927895b9b980ce0d10
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-05-29 10:45:30 -0500

    added cors stuff to startup file

--- MixAndMove/Startup.cs
+++ MixAndMove/Startup.cs
@@ -34,6 +34,7 @@ namespace MixAndMove
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             }
 
             app.UseHttpsRedirection();
