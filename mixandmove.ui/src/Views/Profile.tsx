abef5c8a9c1360bde8b5232a0ddb551aca490642
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-06-02 14:00:30 -0500

    added views and routes

---
+++ mixandmove.ui/src/Views/Profile.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render(): JSX.Element {
+        return (
+            <div>
+                <h1>Profile</h1>
+            </div>
+        )
+    }
+}
+
+export default Profile;
