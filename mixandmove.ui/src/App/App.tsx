d2bbd0819e7beba254675b4fc5aad2d79d15ed4b
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-06-02 14:19:34 -0500

    navbar complete and links to other views

--- mixandmove.ui/src/App/App.tsx
+++ mixandmove.ui/src/App/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../Helpers/routes';
 import './App.scss';
+import Navigation from '../Components/Navigation';
 
 function App(): JSX.Element {
   return (
     <div className="App">
+      <Navigation/>
       <Router>
         <Routes></Routes>
       </Router>
