501bd7ae1d42896036f6a1fc09b6b7e242d19c09
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-06-04 17:13:56 -0500

    working on about view content

--- mixandmove.ui/src/Views/About.tsx
+++ mixandmove.ui/src/Views/About.tsx
@@ -4,7 +4,15 @@ class About extends Component {
     render(): JSX.Element {
         return (
             <div>
-                <h1>About</h1>
+                <div className="bubble">
+                    <h2 className="pb-3">ABOUT MIX + MOVE</h2>
+                    <p>MIX + MOVE is designed to simplify at-home workouts. No weights. No excuses. Start by choosing a workout format, choose your focus areas, and then get moving!</p>
+                    <p>Click on a workout format to learn more.</p>
+                    <div className="d-flex justify-content-center">
+                        <button className="mini-bubble">Time-Based</button>
+                        <button className="mini-bubble">Rep-Based</button>
+                    </div>
+                </div>
             </div>
         )
     }
